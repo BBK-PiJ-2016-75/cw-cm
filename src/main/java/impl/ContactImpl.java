f636a700018fa27b54b2f9ae60e4f2fc34e4ce8b
Dennis Bueso-Calix <dlbcalix@gmail.com>
2017-03-17 20:23:12 +0000

    Added Implementations.

---
+++ src/main/java/impl/ContactImpl.java
@@ -0,0 +1,59 @@
+package impl;
+
+import spec.Contact;
+
+/**
+ * Created by Dennis on 17/03/2017.
+ */
+public class ContactImpl implements Contact {
+  public ContactImpl(String sue, String notes) {
+
+  }
+
+  public ContactImpl(String name) {
+
+  }
+
+  /**
+   * Returns the ID of the contact.
+   *
+   * @return the ID of the contact.
+   */
+  @Override
+  public int getId() {
+    return 0;
+  }
+
+  /**
+   * Returns the name of the contact.
+   *
+   * @return the name of the contact.
+   */
+  @Override
+  public String getName() {
+    return null;
+  }
+
+  /**
+   * Returns our notes about the contact, if any.
+   * <p>
+   * <p>If we have not written anything about the contact, the empty
+   * string is returned.</p>
+   *
+   * @return a string with notes about the contact, maybe empty.
+   */
+  @Override
+  public String getNotes() {
+    return null;
+  }
+
+  /**
+   * Add notes about the contact.
+   *
+   * @param note the notes to be added
+   */
+  @Override
+  public void addNotes(String note) {
+
+  }
+}
---
+++ src/main/java/impl/MeetingImpl.java
@@ -0,0 +1,63 @@
+package impl;
+
+import spec.Contact;
+import spec.Meeting;
+import spec.PastMeeting;
+
+import java.util.Calendar;
+import java.util.Set;
+
+/**
+ * Created by Dennis on 17/03/2017.
+ */
+public class MeetingImpl implements Meeting, PastMeeting {
+  public MeetingImpl(Calendar nowDate, Set<Contact> contacts) {
+
+  }
+
+  /**
+   * Returns the id of the meeting.
+   *
+   * @return the id of the meeting.
+   */
+  @Override
+  public int getId() {
+    return 0;
+  }
+
+  /**
+   * Return the date of the meeting.
+   *
+   * @return the date of the meeting.
+   */
+  @Override
+  public Calendar getDate() {
+    return null;
+  }
+
+  /**
+   * Return the details of people that attended the meeting.
+   * <p>
+   * <p>The list contains a minimum of one contact (if there were
+   * just two people: the user and the contact) and may contain an
+   * arbitrary number of them.</p>
+   *
+   * @return the details of people that attended the meeting.
+   */
+  @Override
+  public Set<Contact> getContacts() {
+    return null;
+  }
+
+  /**
+   * Returns the notes from the meeting.
+   * <p>
+   * <p>If there are no notes, the empty string is returned.</p>
+   *
+   * @return the notes from the meeting.
+   */
+  @Override
+  public String getNotes() {
+    return null;
+  }
+}
